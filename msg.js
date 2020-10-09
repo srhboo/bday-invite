d877fc64a51c61b9a4ec84f06eaa730227983ff4
sarah boo <srhboo@gmail.com>
2020-10-09 00:40:49 -0400

    first push

---
+++ msg.js
@@ -0,0 +1,68 @@
+const msg = `
+<div class="wrapper">
+<section class="chunk">
+<div class="prompt">// HELLO //</div>
+if you are reading this you are invited to my virtual birthday. we will sit in our individual boxes and be together in one big box, eating a meal as friends do. 
+</section>
+
+<section class="chunk">
+<div class="prompt">// WHEN //</div>
+saturday october 10 2020
+year of the beast
+</section>
+
+
+<section class="chunk">
+<div class="prompt">// ITINERARY (EST) //</div>
+<ul>
+<li>
+3:00 pm - come if you're bored. set-up and chat. stream your meal prep.
+</li>
+<li>
+4:00 pm - ***EATING HOUR*** no talking required
+</li>
+<li>
+5:00 pm - more chilling
+</li>
+<li>
+6:00 pm - END
+</li>
+</ul>
+</section>
+
+<section class="chunk">
+<div class="prompt">// WHERE //</div>
+I will post a link to the video chat --> here <-- at 3:00 pm EST on saturday
+</section>
+
+<section class="chunk">
+<div class="prompt">// I DON'T GET IT //</div>
+I think it would be nice to eat a meal with my friends on my birthday. I'd like us all to put on our funkiest costumes, cook our favourite meal, and stream ourselves eating during the ~eating hour~. We don't even have to talk (but who's to say we can't). Feel free to drop in / out whenever, as you please! And come early / stick around after the meal if you feel like it.
+</section>
+
+<section class="chunk">
+<div class="prompt">// ARE WE GONNA HAVE OUR MICS ON WHILE WE EAT? //</div>
+I think it would be fun.
+</section>
+
+<section class="chunk">
+<div class="prompt">// BUT I CAN'T STAND EATING SOUNDS // </div>
+You can do whatever you want.
+</section>
+
+<section class="chunk">
+<div class="prompt">// DO I HAVE TO DRESS UP? //</div>
+No but you should.
+</section>
+
+<section class="chunk">
+<div class="prompt">// CAN I SHOW UP DURING THE EATING HOUR WITH NO FOOD? //</div>
+Yeah whatever.
+</section>
+
+<section class="chunk">
+<div class="prompt">// I WANT TO BE HAPPY //</div>
+Press this <button id="press">button</button>
+</section>
+</div>
+`;
---
+++ sketch.js
@@ -0,0 +1,121 @@
+let myFont;
+let peace;
+let adjustedX;
+let adjustedY;
+let face;
+let view = "msg";
+let message;
+let camera;
+let z = 700,
+  x = 0;
+let orbs = [];
+let colours = ["blue"];
+
+function goPlay() {
+  view = "play";
+  message.hide();
+}
+
+function preload() {
+  myFont = loadFont("assets/VT323.ttf");
+  peace = loadImage("assets/peace.png");
+  face = loadImage("assets/bday-1.png");
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  adjustedX = -width / 2;
+  adjustedY = -height / 2;
+  noStroke();
+  textFont(myFont);
+  message = createDiv(msg).class("msg").attribute("id", "msg");
+  const button = document.getElementById("press");
+  button.addEventListener("click", goPlay);
+  camera = createCamera();
+  setCamera(camera);
+
+  for (let i = 0; i < 100; i++) {
+    const x = random(-5000, 2000);
+    const r1 = random(300, 400);
+    const r2 = random(0, 180);
+    const c = random(colours);
+    orbs.push({ x, r1, r2, c });
+  }
+}
+
+function draw() {
+  background(0);
+  textSize(48);
+  fill(220);
+  if (view === "play") {
+    if (keyIsDown(65)) {
+      x -= 5;
+    }
+
+    if (keyIsDown(68)) {
+      x += 5;
+    }
+
+    if (keyIsDown(87)) {
+      z -= 5;
+    }
+
+    if (keyIsDown(83)) {
+      z += 5;
+    }
+
+    text(
+      "Press WASD to move.",
+      adjustedX + 100,
+      adjustedY + 100,
+      width - 100,
+      height - 100
+    );
+    text(
+      "Follow the peace signs.",
+      adjustedX + 100,
+      adjustedY + 200,
+      width - 100,
+      height - 100
+    );
+    camera.setPosition(x, 0, z);
+
+    push();
+    shininess(200);
+    ambientLight(50);
+    specularColor(200, 160, 100);
+    pointLight(255, 255, 200, 0, -50, 150);
+    specularColor(100, 80, 160);
+    pointLight(0, 255, 0, 0, 50, 150);
+
+    for (let i = 0; i < orbs.length; i++) {
+      push();
+      const { x, r1, r2, c } = orbs[i];
+      translate(x, r1 * sin(r2 + millis() / 2000), -i * 500);
+      specularMaterial(c);
+      sphere(100);
+      pop();
+    }
+    pop();
+
+    push();
+    translate(-400, 0, -1000);
+    image(peace, 0, 0);
+    translate(500, 0, -4000);
+    image(peace, 0, 0);
+    translate(-1000, 0, -5000);
+    image(peace, 0, 0);
+    translate(1000, 0, -6000);
+    image(peace, 0, 0);
+    translate(1000, 0, -6000);
+    image(peace, 0, 0);
+    translate(1000, 0, -6000);
+    image(face, 0, 0);
+    textSize(100);
+    text("you made it! love ya xoxo", -10, 800);
+    pop();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
